50ee3984f59b77dd996f02291a3b78fc5264a58d
WattWeiser <moin@wattweiser.com>
2025-12-22 23:31:42 +0100

    fix(web): handle optional onOpenChange in CommandDialog

--- apps/web/src/components/ui/command-palette.tsx
+++ apps/web/src/components/ui/command-palette.tsx
@@ -29,7 +29,7 @@ Command.displayName = CommandPrimitive.displayName;
 
 const CommandDialog = ({ children, open, onOpenChange }: CommandPaletteProps) => {
   return (
-    <Dialog open={open ?? false} onOpenChange={onOpenChange}>
+    <Dialog open={open ?? false} onOpenChange={onOpenChange ?? undefined}>
       <DialogContent className="overflow-hidden p-0 shadow-lg">
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-gray-500 [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
